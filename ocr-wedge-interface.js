7a22a6e19a7dffa45a85a993c550bb7f0d071aa3
Simone Pozzobon <simone.pozzobon@zebra.com>
2024-10-16 16:09:06 +0200

    automatic import of dw + refactoring broadcast receiver

--- ocr-wedge-interface.js
+++ ocr-wedge-interface.js
@@ -1,84 +1,111 @@
-CURRENT_PROFILE = "DEFAULT"
-
 window.onload = function() {
+    // Download datawedge db file
+    downloadDatawedgeFile()
+
+    // Register to datawedge intents
     startIntentListener()
-    captureBarcode()
+
+    // Force profile switch to default one
+    switchToProfile("barcode", false)
 };
+
+
+function downloadDatawedgeFile() {
+    // Copy Datawedge config to download folder
+    var destination = "/sdcard/Download/datawedge.db";
+    EB.RhoFile.copy("/sdcard/android/data/com.zebra.mdna.enterprisebrowser/datawedge.db", destination)
+    
+    // Send to datawedge import command from download folder
+    var extras = {
+        "FOLDER_PATH": "/sdcard/Download/",
+        "FILE_LIST": [
+            "datawedge.db"
+        ]
+    };
+	var params = {
+		intentType: EB.Intent.BROADCAST,
+		action: 'com.symbol.datawedge.api.ACTION',
+		appName: 'com.symbol.datawedge',
+		data: {
+            "com.symbol.datawedge.api.IMPORT_CONFIG": extras,
+            "SEND_RESULT": "true",
+            "COMMAND_IDENTIFIER": "123456789"
+        }
+	};
+	EB.Intent.send(params);	
+    // hide spinner
+    var divSpinner = document.getElementById("custom-overlay");
+    divSpinner.hidden = true; 
+
+    // Decomment if you want to host this app
+    //  var f = EB.Network.downloadFile({
+    //    url: "./datawedge.db",
+    //    filename: "/enterprise/device/settings/datawedge/autoimport/datawedge.db",
+    //    overwriteFile: true,
+    //    createFolders: true,
+    //    downloadNotification: false
+    //  },downloadCallback);
+}
+
+// Decomment if you want to host this app
+// function downloadCallback(dat) {
+//     if(dat != null && dat.error_code == 200){
+//         var divSpinner = document.getElementById("custom-overlay");
+//         divSpinner.hidden = true; 
+//     } else{
+//         alert('Unable to configure DataWedge! Please restart the app');
+//     } 
+// }
 
 function captureLP(){
     switchToProfile("lp")
-    CURRENT_PROFILE = "lp"
 }
 function captureVIN(){
     switchToProfile("vin")
-    CURRENT_PROFILE = "lp"
 }
 function captureTIN(){
     switchToProfile("tin")
-    CURRENT_PROFILE = "lp"
 }
-function captureBarcode(){
-    switchToProfile("barcode")
-    CURRENT_PROFILE = "DEFAULT"
+function captureId(){
+    switchToProfile("id")
 }
 
 function dwBroadcastReceiver(myIntentData) 
 {
     console.log(myIntentData)
-    // if (myIntentData != null && myIntentData.action == "com.symbol.datawedge.api.NOTIFICATION_ACTION") {
+    if (myIntentData != null && myIntentData.action.startsWith("com.symbol.dw")) 
+	{ 
+        // Get value data
+        let data = myIntentData.data["com.symbol.datawedge.data_string"];
 
-    //     let b = myIntentData["com.symbol.datawedge.api.NOTIFICATION"];
-    //     let NOTIFICATION_TYPE = b["NOTIFICATION_TYPE"];
-    
-    //     if (NOTIFICATION_TYPE !== null) {
-    //         switch (NOTIFICATION_TYPE) {
-    //             case NOTIFICATION_TYPE_SCANNER_STATUS:
-    //                 console.log("SCANNER_STATUS: status: " + b["STATUS"] + ", profileName: " + b["PROFILE_NAME"]);
-    //                 break;
-    
-    //             case NOTIFICATION_TYPE_PROFILE_SWITCH:
-    //                 console.log("PROFILE_SWITCH: profileName: " + b["PROFILE_NAME"] + ", profileEnabled: " + b["PROFILE_ENABLED"]);
-    //                 CURRENT_PROFILE = b["PROFILE_NAME"]
-    //                 startCapture();
-    //                 break;
-    
-    //             case NOTIFICATION_TYPE_CONFIGURATION_UPDATE:
-    //                 // Handle CONFIGURATION_UPDATE if needed
-    //                 break;
-    
-    //             case NOTIFICATION_TYPE_WORKFLOW_STATUS:
-    //                 console.log("WORKFLOW_STATUS: status: " + b["STATUS"] + ", profileName: " + b["PROFILE_NAME"]);
-    //                 break;
-    //         }
-    //     }
-    // }
-    // else
-    if (myIntentData != null && myIntentData.action == "com.symbol.dw.action") 
-	{
-        // Assuming 'intent' contains the data as a string, similar to getStringExtra in Java
-        let data = myIntentData.data;
+        var ouptutDiv;
+        switch (myIntentData.action) {
+            case "com.symbol.dwid.action":
+                ouptutDiv = document.getElementById("id_output");
+                console.log("Identification Document: " + data);
+                break;
+            case "com.symbol.dwlp.action":
+                ouptutDiv = document.getElementById("lp_output");
+                console.log("License Plate: " + data);
+                break;
 
-        if (CURRENT_PROFILE == "lp") {
-            var ouptutDiv = document.getElementById("lp_output");
-            ouptutDiv.innerHTML = data["com.symbol.datawedge.data_string"];
+            case "com.symbol.dwvin.action":
+                ouptutDiv = document.getElementById("vin_output");
+                console.log("VIN (Vehicle Identification Number): " + data);
+                break;
 
-            console.log("License Plate: " + data["com.symbol.datawedge.data_string"]);	
-        } else if (CURRENT_PROFILE == "vin") {
-            var ouptutDiv = document.getElementById("vin_output");
-            ouptutDiv.innerHTML = data["com.symbol.datawedge.data_string"];
+            case "com.symbol.dwtin.action":
+                ouptutDiv = document.getElementById("tin_output");
+                console.log("TIN (Tyre Identification Number): " + data);
+                break;
 
-            console.log("VIN (Vehicle Identification Number): " + data["com.symbol.datawedge.data_string"]);
-        } else if (CURRENT_PROFILE == "tin") {
-            var ouptutDiv = document.getElementById("tin_output");
-            ouptutDiv.innerHTML = data["com.symbol.datawedge.data_string"];
-            console.log("TIN (Tyre Identification Number): " + data["com.symbol.datawedge.data_string"]);
-        } else {
-            // DEFAULT
-            var ouptutDiv = document.getElementById("scanner_output");
-            ouptutDiv.innerHTML = data["com.symbol.datawedge.data_string"];
-            console.log("Scanned Data: " + data["com.symbol.datawedge.data_string"]);
+            default:
+                console.log("AN ERROR OCCURED - WRONG PROFILE");
+                break;
         }
 
+        // Display data
+        ouptutDiv.innerHTML = data;
     }
 	else {
         console.log("Intent action is not registered...");
@@ -87,19 +114,30 @@ function dwBroadcastReceiver(myIntentData)
 
 function startIntentListener(){
     EB.Intent.startListening(dwBroadcastReceiver);
-    // Register for notification
-	// var extras = {
-    //     "com.symbol.datawedge.api.REGISTER_FOR_NOTIFICATION": {
-    //         "com.symbol.datawedge.api.APPLICATION_NAME": "com.zebra.mdna.enterprisebrowser",
-    //         "com.symbol.datawedge.api.NOTIFICATION_TYPE": "PROFILE_SWITCH"
-    //     }
-    // };
-	// sendIntentData(extras);	
-
 	console.log("startListening");
 }
 
+function switchToProfile(profileName, sendTrigger = true){
+	var extras = {
+		"com.symbol.datawedge.api.SWITCH_TO_PROFILE":profileName,
+	};
+	sendIntentData(extras);	
+
+    // Trigger 
+    if(sendTrigger){
+        // Show trigger toast
+        const toastLive = document.getElementById('liveToast')
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLive)
+        toastBootstrap.show()
+
+        setTimeout(function(){
+            startCapture()
+        },1000);  
+    }
+}
+
 function startCapture(){
+    // Trigger scanner/camera upon configuration
     var params = {
         intentType: EB.Intent.BROADCAST,
         action: 'com.symbol.datawedge.api.ACTION_SOFTSCANTRIGGER',
@@ -107,20 +145,6 @@ function startCapture(){
         data: {"com.symbol.datawedge.api.EXTRA_PARAMETER":"START_SCANNING"}
     };
     EB.Intent.send(params);	
-}
-
-function switchToProfile(profileName){
-	var extras = {
-		"com.symbol.datawedge.api.SWITCH_TO_PROFILE":profileName,
-	};
-	sendIntentData(extras);	
-
-    const toastLive = document.getElementById('liveToast')
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLive)
-    toastBootstrap.show()
-    setTimeout(function(){
-        startCapture()
-   },1000);  
 }
 
 function sendIntentData(extraData)
